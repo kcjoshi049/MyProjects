560b37c18c036a791715359f7464fc7a4007836e
Kamal chandra joshi <kj144049@gmail.com>
2025-01-14 18:27:11 +0530

    some changes is added

---
+++ MusicApp/Song/Array.js
@@ -0,0 +1,82 @@
+export const playlists = [
+  {//0
+    description: {
+      id: "Liked",
+      playlistName: "liked",
+      author: "Kamal chandra joshi",
+      img_url: "https://media.istockphoto.com/id/1903985199/vector/heart-flat-icon.jpg?s=612x612&w=0&k=20&c=FOAQ1kofne38dLzm6C6vBE39nY9fynVBpQb7UcDW7gk="
+    },
+    songs: [
+      {
+        // id: "Liked_song1",
+        name: "agar tum saath ho",
+        duration: { min: 5, sec: 41 },
+        singers: { male: "arijit singh,", female: "alka yagnik" },
+        id: "./Song/Liked songs/agar-tum-saath-ho.mp3",
+        img_url:
+          "https://i.pinimg.com/736x/5f/83/9d/5f839d3b4ee6c8a6de9d5620bfa91826.jpg"
+      },
+      {
+        // id: "Liked_song2",
+        name: "kal chaudhvi ki raat",
+        duration: { min: 8, sec: 6 },
+        singers: { male: "jagjit singh", female: "" },
+        id: "./Song/Liked songs/kal chaudhvi ki raat.mp3",
+        img_url: "https://i.ytimg.com/vi/rn-oFhieEOw/hq720.jpg?sqp=-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAElAADIQj0AgKJD8AEB-AH-CYAC0AWKAgwIABABGH8gPCgrMA8=&rs=AOn4CLBJbzpQeaXeET0_vHD9-BjFXNvCJw"
+      },
+    ],
+  },
+  {
+    description: {//1
+      id: "Arijit",
+      playlistName: "arijit singh",
+      author: "spotify",
+      img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3ZHsVvr_-02HFFfsP8Ufz6RvK7qQXY5if_Q&s",
+    },
+    songs: [
+      {
+        // id: "Arijitsong1",
+        name: "agar tum saath ho",
+        duration: { min: 5, sec: 41 },
+        singers: { male: "arijit singh,", female: "alka yagnik" },
+        id: "./Song/Arijit_singh/agar-tum-saath-ho.mp3",
+        img_url:
+          "https://i.pinimg.com/736x/5f/83/9d/5f839d3b4ee6c8a6de9d5620bfa91826.jpg",
+      },
+      {
+        // id: "Arijitsong2",
+        name: "phir le aaya dil",
+        duration: { min: 5, sec: 5 },
+        singers: { male: "arijit singh", female: "" },
+        id: "./Song/Arijit_singh/phir le aaya dil.mp3",
+        img_url:
+          "https://i.scdn.co/image/ab67616d0000b273352f44aed6f226c6ea28b961"
+      },
+      {
+        name:"aayat",
+        duration:{min:4, sec:22},
+        singers:{male:"arijit singh", female:""},
+        id : "./Song/Arijit_singh/aayat.mp3",
+        img_url : "https://m.media-amazon.com/images/M/MV5BNTk0ZWY2ODQtM2E5OS00YjRiLThjNGMtYWU0NzAxMDEwN2MzXkEyXkFqcGc@._V1_QL75_UX480_.jpg"
+      }
+    ],
+  },
+  {
+    description:{//3
+      id: "Ghazals",
+      playlistName: "all time best Ghazals",
+      author: "kamal chandra joshi",
+      img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSJQeG0op9E7xPPcGe97LmFVYdH2pGQ1Vd2g&s",
+    },
+    songs:[
+      {
+        name: "hum tere shahar mein",
+        duration: { min: 7, sec: 7 },
+        singers: { male: "gulam ali", female: "" },
+        id: "./song/Ghazals/Hum tere shahar me.mp3",
+        img_url:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1GCGb5HYFQQmaPDu3PhEeOxKw1rSZbIJ1hA&s",
+      }
+    ]
+  }
+];
---
+++ MusicApp/script.js
@@ -0,0 +1,232 @@
+import { playlists } from "./Song/Array.js";
+
+function add_song(src, name, artists, duration, id) {
+  let elem = document.createElement("div");
+  elem.className = "card";
+  elem.id = id;
+  function minutecheck(x) {
+    if (x.min < 10) {
+      return `0${x.min}`;
+    }
+    return x.min;
+  }
+  function secondCheck(x) {
+    if (x.sec < 10) {
+      return `0${x.sec}`;
+    }
+    return x.sec;
+  }
+  elem.innerHTML = `
+            <img
+              src=${src}
+            />
+            <div class="dis">
+              <h4>${name}</h4>
+              <h6>${artists.male}${artists.female}</h6>
+            </div>
+            <div class="time">
+            <i class="fa-solid fa-play"></i>
+            <h5>${minutecheck(duration)}:${secondCheck(duration)}</h5>
+            </div>`;
+  document.querySelector(".playlist").append(elem);
+}
+
+function Add_playlist(li_name, li_author, li_img, id) {
+  let elem = document.createElement("div");
+  elem.className = "list-items";
+  elem.id = id;
+  elem.innerHTML = `
+            <img src=${li_img} />
+            <div class="list-name">
+              <h4>${li_name}</h4>
+              <h5>by - ${li_author}</h5>
+            </div>`;
+  document.querySelector(".playlist_0").append(elem);
+}
+
+for (const i of playlists) {
+  let x = i.description;
+  Add_playlist(x.playlistName, x.author, x.img_url, x.id);
+}
+
+// Event handling on playlists
+// Eventlistener for Liked playlist
+function addingSong(num) {
+  // function to add songs
+  playlists[num].songs.forEach((elem) => {
+    add_song(
+      elem.img_url,
+      elem.name,
+      elem.singers,
+      elem.duration,
+      elem.id,
+      elem.url
+    );
+  });
+}
+function removingSong() {
+  // function to remove songs
+  document.querySelector(".playlist").innerHTML = "";
+}
+let currentSelectedId = null;
+document.querySelector(".playlist_0").addEventListener("click", (event) => {
+  if (event.target.classList.contains("list-items")) {
+    let other_items = document.querySelectorAll(".list-items");
+    other_items.forEach((elem) => {
+      elem.style.backgroundColor = "rgba(0, 0, 0, 0.636)";
+    });
+    event.target.style.backgroundColor = "rgb(103 87 87 / 64%)";
+    if (currentSelectedId === event.target.id) {
+      return;
+    }
+    currentSelectedId = event.target.id;
+    removingSong();
+    switch (event.target.id) {
+      case "Liked":
+        {
+          document.getElementById("heart").classList.replace("fa-regular","fa-solid");
+          document.getElementById("heart").style.color = "red";
+          addingSong(0);
+          // removingSong(1);
+          // removingSong(2);
+        }
+        break;
+      case "Arijit":
+        {
+          document.getElementById("heart").classList.replace("fa-solid","fa-regular");
+          document.getElementById("heart").style.color = "white";
+          addingSong(1);
+          // removingSong(0);
+          // removingSong(2);
+        }
+        break;
+      case "Ghazals":
+        {
+          document.getElementById("heart").classList.replace("fa-solid","fa-regular");
+          document.getElementById("heart").style.color = "white";
+          addingSong(2);
+          // removingSong(0);
+          // removingSong(1);
+        }
+        break;
+    }
+  }
+});
+let currentSong = null;
+let currentUrl = null;
+let currentTarget = null; // this will handle the play button of the previous selected song //
+let play_btn = document.getElementById("playbutton");
+// handling songs from here
+document.querySelector(".playlist").addEventListener("click", (event) => {
+  if (event.target.classList.contains("card")) {
+    let cards = document.querySelectorAll(".card");
+
+    cards.forEach((card) => {
+      card.style.backgroundColor = "rgba(0, 0, 0, 0.421)";
+    });
+
+    // changing track details//
+
+    document.getElementById("playtrack-img").src =
+      event.target.firstElementChild.src;
+    document.getElementById("marquee").textContent =
+      event.target.children[1].firstElementChild.textContent;
+    document.getElementById("artists_name").textContent =
+      event.target.children[1].lastElementChild.textContent;
+    //
+    event.target.style.backgroundColor = "black";
+
+    //
+
+    let url = event.target.id;
+
+    if (currentUrl === url) {
+      return;
+    }
+    if (currentSong) {
+      currentSong.pause();
+      currentSong.currentTime = 0;
+      play_btn.classList.replace("fa-pause","fa-play");
+      // play button of targeted element
+      if (currentTarget) {
+        currentTarget.classList.replace("fa-pause", "fa-play");
+        currentTarget.onclick = null; // Remove the previous handler
+      }
+      document.getElementById("playtrack-img").style.animation = "none";
+      //
+    }
+    // function to update the play buttons and track disk
+    function updateButtons() {
+      if (currentSong.paused) {
+        currentSong.play();
+        document.getElementById("playtrack-img").style.animation =
+          "rotation 10s linear infinite";
+        play_btn.classList.replace("fa-play","fa-pause");
+        // play button of targeted element
+        event.target.lastElementChild.firstElementChild.classList.replace("fa-play","fa-pause");
+        //
+      } else {
+        currentSong.pause();
+        play_btn.classList.replace("fa-pause","fa-play");
+        // play button of targeted element
+        event.target.lastElementChild.firstElementChild.classList.replace("fa-pause","fa-play");
+        //
+        document.getElementById("playtrack-img").style.animation = "none";
+      }
+    }
+    // update play buttons and track disk ends here
+    currentSong = new Audio(url);
+    currentUrl = url;
+    let seekbar = document.getElementById("Seek");
+    currentTarget = event.target.lastElementChild.firstElementChild;
+    
+    play_btn.onclick = () => {updateButtons()}; // updatebuttons function calls here //
+    event.target.lastElementChild.firstElementChild.onclick = () =>{updateButtons()}; // updatebuttons functions will not calls here // 
+
+    // updating the seek bar
+    // setting time section
+    let timeFormat = (time) => {
+      // this function will convert our time into second and minute
+      let min = Math.floor(time / 60)
+        .toString()
+        .padStart(2, "0");
+      let sec = Math.floor(time % 60)
+        .toString()
+        .padStart(2, "0");
+      return `${min}:${sec}`;
+    };
+    currentSong.addEventListener("loadedmetadata", () => {
+      document.querySelector(".s2").textContent = timeFormat(
+        currentSong.duration
+      );
+      seekbar.value = 0;
+    });
+    //
+    currentSong.addEventListener("timeupdate", () => {
+      document.querySelector(".s1").textContent = timeFormat(
+        currentSong.currentTime
+      );
+      let progress = parseInt(
+        (currentSong.currentTime / currentSong.duration) * 100
+      );
+      seekbar.value = progress;
+      console.log(progress);
+      console.log(currentSong.currentTime);
+      if (progress == 100) {
+        play_btn.classList.add("fa-play");
+        play_btn.classList.remove("fa-pause");
+        event.target.lastElementChild.firstElementChild.classList.add(
+          "fa-play"
+        );
+        event.target.lastElementChild.firstElementChild.classList.remove(
+          "fa-pause"
+        );
+      }
+    });
+    seekbar.addEventListener("change", () => {
+      currentSong.currentTime = (seekbar.value * currentSong.duration) / 100;
+    });
+  }
+});
+// From here our song will be play
+// Play audio
