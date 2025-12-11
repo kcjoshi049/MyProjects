b36596e17690d72135fe52ae03338b9142f71f9a
Kamal chandra joshi <kj144049@gmail.com>
2025-12-11 19:45:51 +0530

    server.js modefied

--- FoodApp/Food_delivery_app/server/server.js
+++ FoodApp/Food_delivery_app/server/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/Food").then(()=>{
+mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log("the database connected successfully.");
 }).catch((err)=>{
     console.error("error :- ",err);
